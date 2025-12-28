80bc8198a344a0648938c2719418311ab14b7a32
BulatovicBalsa <baki.b.2k02@gmail.com>
2025-12-28 11:42:09 +0100

    feat: Create Password change view class

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.contrib.auth.views import PasswordChangeView
+from django.urls import reverse_lazy
+from django.utils import timezone
 
-# Create your views here.
+from .models import User
+
+
+class ForcedPasswordChangeView(PasswordChangeView):
+    """
+    Same as Django PasswordChangeView, but after successful change:
+    - clears must_change_password
+    - sets last_password_change_at (if present)
+    """
+    template_name = "registration/password_change_form.html"
+    success_url = reverse_lazy("password_change_done")
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        user: User = self.request.user
+        if getattr(user, "must_change_password", False):
+            user.must_change_password = False
+            if hasattr(user, "last_password_change_at"):
+                user.last_password_change_at = timezone.now()
+            user.save(update_fields=[
+                "must_change_password",
+                * (["last_password_change_at"] if hasattr(user, "last_password_change_at") else [])
+            ])
+        return response
