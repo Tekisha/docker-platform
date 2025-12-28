c28afa5a84e4dc87562e3f3f82b7faef99b0991d
BulatovicBalsa <baki.b.2k02@gmail.com>
2025-12-28 12:32:27 +0100

    feat: Register user list urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+
+from . import views_admin
 from .views import ForcedPasswordChangeView
 
 urlpatterns = [
@@ -9,4 +11,8 @@ urlpatterns = [
     # Forced password change route
     path("password/change/", ForcedPasswordChangeView.as_view(), name="password_change"),
     path("password/change/done/", auth_views.PasswordChangeDoneView.as_view(), name="password_change_done"),
+
+    path("admin/users/", views_admin.admin_user_list, name="admin_user_list"),
+    path("admin/users/<uuid:user_id>/publisher-status/", views_admin.set_publisher_status, name="set_publisher_status"),
+
 ]
